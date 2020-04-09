4f200d70d5c6d2de3b0a4a1939df4317fea96a14
William Eisert <weisert@eisertdev.com>
2020-04-09 00:32:27 -0500

    Update apollo.js

--- app/javascript/services/apollo.js
+++ app/javascript/services/apollo.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: `${window.location.origin}/graphql`,
 });
 
 export default client
